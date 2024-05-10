a6ae795488aa189519f8bdc813ea63a6646dc36e
Shlok <shlok@C02J5E7HQ6L4.local>
2024-05-10 09:12:47 +0530

    Added coupon feature

--- my_dgp_backend/routes/CouponRoutes.js
+++ my_dgp_backend/routes/CouponRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/Auth");
 const {
   getAllCoupons,
   createCoupon,
