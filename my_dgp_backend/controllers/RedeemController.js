c82d1e0aa16bf1e62129cdc67a83627c5bf27eba
Shlok <shlok@C02J5E7HQ6L4.local>
2024-05-19 22:30:59 +0530

    Updated Redeem API to give earning details

--- my_dgp_backend/controllers/RedeemController.js
+++ my_dgp_backend/controllers/RedeemController.js
@@ -15,26 +15,65 @@ exports.getRedeemDetails = catchAsyncErrors(async (req, res, next) => {
   currentDate.setHours(0, 0, 0, 0);
   let nextDay = new Date(currentDate);
   nextDay.setDate(currentDate.getDate() + 1);
+  
+  let todayMinutesServiced = 0, lastMonthMinutesServiced = 0, totalEarnings = 0, todayEarnings = 0, lastMonthEarnings = 0
+  let lastMonthDate = new Date(currentDate - 1000 * 60 * 60 * 24 * 30) // date 30 days back
 
+  // today's minutes serviced & earnings
   let bookings = await Booking.find({
     serviceProvider: req.user._id,
     date: {
       $gte: currentDate,
       $lt: nextDay,
     },
-    status: Enums.BOOKING_STATUS.CLOSED,
+    status: [Enums.BOOKING_STATUS.CLOSED, Enums.BOOKING_STATUS.COMPLETED],
   });
-
-  let todayMinutesServiced = 0;
   bookings.map(
     (booking) =>
-      todayMinutesServiced + (booking.hours * 60 + (booking.minutes || 0))
+      {
+        todayMinutesServiced += booking.hours * 60 + (booking.minutes || 0)
+        todayEarnings += 10 // giving 10 Rupees to service_provider for every booking
+      }
   );
+
+  // last month minutes serviced & earnings
+  bookings = await Booking.find({
+    serviceProvider: req.user._id,
+    date: {
+      $gte: lastMonthDate,
+      $lte: currentDate,
+    },
+    status: [Enums.BOOKING_STATUS.CLOSED, Enums.BOOKING_STATUS.COMPLETED],
+  });
+  bookings.map(
+    (booking) =>
+      {
+        lastMonthMinutesServiced += booking.hours * 60 + (booking.minutes || 0)
+        lastMonthEarnings += 10
+      }
+  );
+
+  // total Earnings
+  bookings = await Booking.find({
+    serviceProvider: req.user._id,
+    status: [Enums.BOOKING_STATUS.CLOSED, Enums.BOOKING_STATUS.COMPLETED],
+  })
+  bookings.map(booking => {
+    totalEarnings += 10
+  })
 
   res.status(200).json({
     success: true,
     redeem,
-    totalMinutesServiced: service_provider?.minutesServiced || 0,
-    todayMinutesServiced
+    minutesServiced: {
+      today: todayMinutesServiced,
+      lastMonth: lastMonthMinutesServiced,
+      total: service_provider?.minutesServiced || 0,
+    },
+    earnings: {
+      today: todayEarnings,
+      lastMonth: lastMonthEarnings,
+      total: totalEarnings
+    }
   });
 });
