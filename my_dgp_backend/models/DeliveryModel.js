cc6607d9a23f0e05d2d5d0fb5c45c69e7cff95b4
Garvik <apple@Apples-MacBook-Pro.local>
2024-07-28 03:05:04 +0530

    ++delivery request approval

--- my_dgp_backend/models/DeliveryModel.js
+++ my_dgp_backend/models/DeliveryModel.js
@@ -4,7 +4,7 @@ const deliverySchema = new mongoose.Schema({
     _id: String,
     deliveryUrl: String,
     contactNumber: String,
-    isApproved: boolean
+    isApproved: Boolean
 });
 
 module.exports = mongoose.model('DeliveryRequest', deliverySchema);
