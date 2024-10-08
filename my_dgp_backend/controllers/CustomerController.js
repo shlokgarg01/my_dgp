6906799e20ea4a53b271bbbc1cb830768d0401f4
Garvik <apple@Apples-MacBook-Pro.local>
2024-10-09 00:45:37 +0530

    fix

--- my_dgp_backend/controllers/CustomerController.js
+++ my_dgp_backend/controllers/CustomerController.js
@@ -1,35 +1,11 @@
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/CatchAsyncErrors");
 const Customer = require("../models/CustomerModel");
-const Package = require("../models/PackageModel");
-const Redeem = require("../models/RedeemModel");
-const Booking = require("../models/BookingModel");
-const sendToken = require("../utils/JwtToken");
-const Enums = require("../utils/Enums");
 const { getDaysFromCreatedAt } = require("../utils/orderUtils");
 const { uploadFile } = require("../services/googleAPI");
 
-// send OTP for registration
-exports.sendOTPForRegistration = catchAsyncErrors(async (req, res, next) => {
-  const { contactNumber } = req.body;
-  const user = await User.findOne({ contactNumber });
-  if (user) {
-    return next(
-      new ErrorHandler(
-        "User with this Contact Number already exists. Please login.",
-        400
-      )
-    );
-  }
-
-  res.status(200).json({
-    success: true,
-    message: "OTP can be sent.",
-  });
-});
-
-// register user via OTP
-exports.registerCustomer = catchAsyncErrors(async (req, res, next) => {
+// register customer
+exports.registerCustomer = catchAsyncErrors(async (req, res) => {
   const { name, email, contactNumber } = req.body;
 
   const customer = await Customer.create({
@@ -46,65 +22,38 @@ exports.registerCustomer = catchAsyncErrors(async (req, res, next) => {
 //   sendToken(customer, 201, res);
 });
 
-// send OTP for Login
-exports.sendOTPForLogin = catchAsyncErrors(async (req, res, next) => {
-  const { contactNumber } = req.body;
-  const user = await User.findOne({ contactNumber });
-  if (!user) {
-    return next(
-      new ErrorHandler(
-        "User with this Contact Number does not exist. Please Register.",
-        400
-      )
-    );
-  }
-
-  res.status(200).json({
-    success: true,
-    message: "User Present. OTP can be sent",
-  });
-});
-
-// autheticate the User for Login
-exports.authenticateUserViaOTPForLogin = catchAsyncErrors(
-  async (req, res, next) => {
-    const { contactNumber } = req.body;
-    let user = await User.findOne({ contactNumber });
-    if (!user) {
-      return next(
-        new ErrorHandler("No user found for the given Contact Number.", 404)
-      );
+exports.isCustomerRegistered = catchAsyncErrors(
+    async (req, res) => {
+      const { contactNumber } = req.body;
+      let user = await Customer.findOne({ contactNumber });
+      if (!user) {
+        res.status(200).json({
+            success: true,
+            isRegistered:false,
+        });
+      }
+  
+      res.status(200).json({
+        success: true,
+        isRegistered:true,
+        user
+    });
     }
-
-    sendToken(user, 201, res);
-  }
-);
+  );
 
 // get customer details
 exports.getCustomerDetails = catchAsyncErrors(async (req, res, next) => {
-  let customer = await Customer.findById(req.customer.id);
-//   if (customer) {
-//     customer = {
-//       ...customer._doc,
-//     };
-//   }
+    const { contactNumber } = req.body;
+    let customer = await Customer.findOne({ contactNumber });
 
-  res.status(200).json({
-    success: true,
-    customer,
-  });
-});
+    if (!customer) {
+        return next(
+            new ErrorHandler("No user found for the given Contact Number.", 404)
+        );
+    }
 
-// update customer
-exports.updateCustomer = catchAsyncErrors(async (req, res, next) => {
-  const customer = await Customer.findByIdAndUpdate(req.customer.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-
-  res.status(200).json({
-    success: true,
-    customer,
-  });
+    res.status(200).json({
+        success: true,
+        customer,
+    });
 });
--- my_dgp_backend/routes/CustomerRoutes.js
+++ my_dgp_backend/routes/CustomerRoutes.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const {
   registerCustomer,
-  sendOTPForRegistration,
-  sendOTPForLogin,
-  authenticateUserViaOTPForLogin,
-  updateCustomer,
+  isCustomerRegistered,
   getCustomerDetails,
 } = require("../controllers/CustomerController");
 const { isAuthenticatedUser } = require("../middleware/Auth");
 
 const router = express.Router();
 
-// router.route("/register/otp/send").post(sendOTPForRegistration);
 router.route("/customer/register").post(registerCustomer);
-
-// router.route("/login/otp/send").post(sendOTPForLogin);
-// router.route("/login/otp/verify").post(authenticateUserViaOTPForLogin);
-
-router.route("/customer").get(isAuthenticatedUser, getCustomerDetails);
-router.route("/customer/update").put(isAuthenticatedUser, updateCustomer);
+router.route("/customer/isExist").get(isCustomerRegistered);
+router.route("/customer").get( getCustomerDetails);
 
 module.exports = router;
