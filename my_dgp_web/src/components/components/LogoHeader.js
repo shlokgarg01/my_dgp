f871c14e879942c898d58381a444f540620f818c
Garvik Satija <garviks09@gmail.com>
2024-12-16 21:59:22 +0530

    header logo alignment

--- my_dgp_web/src/components/components/LogoHeader.js
+++ my_dgp_web/src/components/components/LogoHeader.js
@@ -1,42 +1,80 @@
-import React from "react";
+import React, { useCallback } from "react";
+import PropTypes from 'prop-types';
 import Logo from "../../images/logo_white.JPG";
-import { IoIosArrowDropleftCircle  } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import Colors from "../../utils/Colors.js";
 
-export default function LogoHeader({ showLogo, backAction,heading }) {
+const containerStyle = {
+  marginBottom: 10,
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: 'center',
+  alignItems: "center",
+};
+
+const backButtonStyle = {
+  position: 'absolute',
+  left: 10,
+};
+
+const logoStyle = {
+  height: 60,
+  width: 60,
+  borderRadius: 100,
+};
+
+const headingStyle = {
+  flex: 1,
+  textAlign: 'center',
+  marginLeft: -12,
+};
+
+const LogoHeader = ({ showLogo, backAction, heading }) => {
   const navigate = useNavigate();
+  
+  const handleBack = useCallback(() => {
+    if (backAction) {
+      backAction();
+    } else {
+      navigate(-1);
+    }
+  }, [backAction, navigate]);
+
   return (
-    <div
-      style={{
-        marginBottom: 10,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-        <IoIosArrowDropleftCircle 
-          onClick={() => backAction ? backAction():navigate(-1)} 
+    <div style={containerStyle}>
+      <div style={backButtonStyle}>
+        <IoIosArrowDropleftCircle
+          onClick={handleBack}
           size={30}
           color={Colors.BLACK}
         />
+      </div>
       {showLogo && (
         <img
           src={Logo}
-          style={{
-            marginRight: '44%',
-            // margin: "0 auto",
-            height: 60,
-            width: 60,
-            borderRadius: 100,
-          }}
-          alt=""
+          style={logoStyle}
+          alt="Logo"
+          loading="lazy"
         />
       )}
       {heading && (
-        <div style={{flex:1,textAlign:'center',marginLeft:-12}} >{heading}</div>
+        <div style={headingStyle}>{heading}</div>
       )}
     </div>
   );
-}
+};
+
+LogoHeader.propTypes = {
+  showLogo: PropTypes.bool,
+  backAction: PropTypes.func,
+  heading: PropTypes.string,
+};
+
+LogoHeader.defaultProps = {
+  showLogo: false,
+  backAction: null,
+  heading: '',
+};
+
+export default React.memo(LogoHeader);
