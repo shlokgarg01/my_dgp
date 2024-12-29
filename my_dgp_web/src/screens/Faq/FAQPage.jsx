7b79af81299f948c9824b4a61e0fdb4f775675a8
Garvik Satija <garviks09@gmail.com>
2024-12-29 21:19:16 +0530

    faq design fix

--- my_dgp_web/src/screens/Faq/FAQPage.jsx
+++ my_dgp_web/src/screens/Faq/FAQPage.jsx
@@ -3,50 +3,71 @@ import { FAQDATA } from './faq-data';
 import HamburgerMenu from '../../components/components/HamburgerMenu';
 
 const FAQPage = ({ faqDatas }) => {
-  const [faqData, setFaqData] = useState(FAQDATA);
-
-  return (
-    <>
-        <HamburgerMenu />
-        <div className='static-page-container'>
-        <h1 style={{textAlign:"center"}}>FAQ</h1>
-
-        {faqData.map((section, index) => (
-            <div key={index} className="faq-section mb-4">
-            <h2>{section.heading}</h2>
-            <div className="accordion" id={`accordion-${index}`}>
-                {section.content.map((faq, idx) => (
-                <div key={idx} className="accordion-item">
-                    <h2 className="accordion-header" id={`heading-${index}-${idx}`}>
-                    <button
-                        className="accordion-button"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target={`#collapse-${index}-${idx}`}
-                        aria-expanded="true"
-                        aria-controls={`collapse-${index}-${idx}`}
-                    >
-                        {faq.question}
-                    </button>
-                    </h2>
-                    <div
-                    id={`collapse-${index}-${idx}`}
-                    className="accordion-collapse collapse"
-                    aria-labelledby={`heading-${index}-${idx}`}
-                    data-bs-parent={`#accordion-${index}`}
-                    >
-                    <div className="accordion-body">
-                        {faq.answer}
-                    </div>
-                    </div>
-                </div>
-                ))}
-            </div>
-            </div>
-        ))}
-        </div>
-    </>
-  );
-};
-
+    const [faqData, setFaqData] = useState(FAQDATA);
+  
+    return (
+      <>
+          <HamburgerMenu />
+          <div className='static-page-container'>
+          <h1 style={{textAlign:"center"}}>FAQ</h1>
+  
+          {faqData.map((section, index) => (
+              <div key={index} className="faq-section mb-4">
+              <div className="accordion" id={`accordion-section-${index}`}>
+                  <h2 className="accordion-header" id={`heading-section-${index}`}>
+                      <button
+                          className={`accordion-button ${false ? '' : 'collapsed'}`}
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target={`#collapse-section-${index}`}
+                          aria-expanded="false"
+                          aria-controls={`collapse-section-${index}`}
+                      >
+                          {section.heading}
+                      </button>
+                  </h2>
+                  <div
+                      id={`collapse-section-${index}`}
+                      className="accordion-collapse collapse"
+                      aria-labelledby={`heading-section-${index}`}
+                      data-bs-parent={`#accordion-section-${index}`}
+                  >
+                      <div className="accordion-body">
+                          <div className="accordion" id={`accordion-${index}`}>
+                              {section.content.map((faq, idx) => (
+                              <div key={idx} className="accordion-item">
+                                  <h2 className="accordion-header" id={`heading-${index}-${idx}`}>
+                                  <button
+                                      className={`accordion-button ${false ? '' : 'collapsed'}`}
+                                      type="button"
+                                      data-bs-toggle="collapse"
+                                      data-bs-target={`#collapse-${index}-${idx}`}
+                                      aria-expanded="false"
+                                      aria-controls={`collapse-${index}-${idx}`}
+                                  >
+                                      {faq.question}
+                                  </button>
+                                  </h2>
+                                  <div
+                                  id={`collapse-${index}-${idx}`}
+                                  className="accordion-collapse collapse"
+                                  aria-labelledby={`heading-${index}-${idx}`}
+                                  data-bs-parent={`#accordion-${index}`}
+                                  >
+                                  <div className="accordion-body">
+                                      {faq.answer}
+                                  </div>
+                                  </div>
+                              </div>
+                              ))}
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              </div>
+          ))}
+          </div>
+      </>
+    );
+  };
 export default FAQPage;
